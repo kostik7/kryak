7b612cd104239cc736d068e616e00652b7681b50
kostik7 <34090519+kostik7@users.noreply.github.com>
2017-11-29 12:55:11 +0200

    Update kryak.js

--- kryak.js
+++ kryak.js
@@ -1,4 +1,25 @@
-var smileys = {":)": "https://emojicodes.com/wp-content/uploads/2014/02/smile.png",":(":"http://emojicodes.com/wp-content/uploads/2014/02/frown.png",":P":"http://emojicodes.com/wp-content/uploads/2014/02/tongue.png"}
+var smileys = 
+    {
+     ":)": "https://emojicodes.com/wp-content/uploads/2014/02/smile.png",
+     ":(":"http://emojicodes.com/wp-content/uploads/2014/02/frown.png",
+     ":P":"http://emojicodes.com/wp-content/uploads/2014/02/tongue.png",
+     ":D":"http://emojicodes.com/wp-content/uploads/2014/02/grin.png",
+     ":O":"http://emojicodes.com/wp-content/uploads/2014/02/gasp.png", 
+     ";)":"http://emojicodes.com/wp-content/uploads/2014/02/wink.png",
+     "B-)":"http://emojicodes.com/wp-content/uploads/2014/02/glasses.png",
+     "B|":"http://emojicodes.com/wp-content/uploads/2014/02/sunglasses.png",
+     ">:(":"http://emojicodes.com/wp-content/uploads/2014/02/grumpy.png",
+     ":/":"http://emojicodes.com/wp-content/uploads/2014/02/unsure.png",
+     ":'(":"http://emojicodes.com/wp-content/uploads/2014/02/cry.png",
+     "3:)":"http://emojicodes.com/wp-content/uploads/2014/02/devil.png",
+     "O:)":"http://emojicodes.com/wp-content/uploads/2014/02/angel.png",
+     ":*":"http://emojicodes.com/wp-content/uploads/2014/02/kiss.png",
+     "<3":"http://emojicodes.com/wp-content/uploads/2014/02/heart.png",
+     "^_^":"http://emojicodes.com/wp-content/uploads/2014/02/kiki.png",
+     "-_-":"http://emojicodes.com/wp-content/uploads/2014/02/squint.png",
+     "o.O":"http://emojicodes.com/wp-content/uploads/2014/02/confused.png",
+     "(y)":"http://emojicodes.com/wp-content/uploads/2014/02/like.png"
+    }
 
 jQuery(document).ready(function() {
         jQuery("li.comment p").each(function( index ) {
