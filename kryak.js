93cf2647ee82110bc2a65b98f12d3c66d7886cf0
kostik7 <34090519+kostik7@users.noreply.github.com>
2017-11-29 12:38:06 +0200

    Update kryak.js

--- kryak.js
+++ kryak.js
@@ -1,29 +1,14 @@
-var smileys = {":)": "https://emojicodes.com/wp-content/uploads/2014/02/smile.png",":(":"http://emojicodes.com/wp-content/uploads/2014/02/frown.png",":P":"http://emojicodes.com/wp-content/uploads/2014/02/tongue.png"}
-
-var smileys_bar = "";
-
-for(key in smileys) {
-      smileys_bar += key + ' - <img src="'+ smileys[key] + '" alt="' + key + '" />';
-}
-
-function smileys_bar_func() {
-      console.log(999999);
-        if(jQuery(".comment-replybox-single").find("#smileys-bar").length == 0) {
-              
-                jQuery(".comment-replybox-single").prepend("<div id='smileys-bar'>Скопируйте смайлик в сообщение: " + smileys_bar + "</div>");
-        }
-        
-        jQuery("#smileys-bar img").on("drop", function(event){
-                console.log(event);
-        });
-}
-
 jQuery(document).ready(function() {
         jQuery("li.comment p").each(function( index ) {
-          jQuery(this).html(jQuery(this).html().replace(":)", '<img src="https://emojicodes.com/wp-content/uploads/2014/02/smile.png" />'));
-          jQuery(this).html(jQuery(this).html().replace(":(", '<img src="http://emojicodes.com/wp-content/uploads/2014/02/frown.png" />'));
-          jQuery(this).html(jQuery(this).html().replace(":P", '<img src="http://emojicodes.com/wp-content/uploads/2014/02/tongue.png" />'));
+          for(key in smileys) {
+                jQuery(this).html(jQuery(this).html().replace(key, smileys[key]));
+          }
         });
-        
-        jQuery("a[kind='i']").click(function() {setTimeout(smileys_bar_func, 1500);});
+ 
+            var smileys = {":)": "https://emojicodes.com/wp-content/uploads/2014/02/smile.png",":(":"http://emojicodes.com/wp-content/uploads/2014/02/frown.png",":P":"http://emojicodes.com/wp-content/uploads/2014/02/tongue.png"}
+            var smileys_bar = "";
+            for(key in smileys) {
+                  smileys_bar += key + ' - <img src="'+ smileys[key] + '" alt="' + key + '" />';
+            }
+            jQuery("p.comment-footer").before("<div>" + smileys_bar + "</div>");
       });
