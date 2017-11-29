28bcffcb67bd3557fe2574521d73034e052d2649
kostik7 <34090519+kostik7@users.noreply.github.com>
2017-11-29 12:41:30 +0200

    Update kryak.js

--- kryak.js
+++ kryak.js
@@ -1,11 +1,11 @@
+var smileys = {":)": "https://emojicodes.com/wp-content/uploads/2014/02/smile.png",":(":"http://emojicodes.com/wp-content/uploads/2014/02/frown.png",":P":"http://emojicodes.com/wp-content/uploads/2014/02/tongue.png"}
+
 jQuery(document).ready(function() {
         jQuery("li.comment p").each(function( index ) {
           for(key in smileys) {
                 jQuery(this).html(jQuery(this).html().replace(key, smileys[key]));
           }
         });
- 
-            var smileys = {":)": "https://emojicodes.com/wp-content/uploads/2014/02/smile.png",":(":"http://emojicodes.com/wp-content/uploads/2014/02/frown.png",":P":"http://emojicodes.com/wp-content/uploads/2014/02/tongue.png"}
             var smileys_bar = "";
             for(key in smileys) {
                   smileys_bar += key + ' - <img src="'+ smileys[key] + '" alt="' + key + '" />';
