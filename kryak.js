faa88649ae69227d865d0dca5abd1ffac3661c6e
kostik7 <34090519+kostik7@users.noreply.github.com>
2017-11-29 11:25:16 +0200

    Update kryak.js

--- kryak.js
+++ kryak.js
@@ -1,5 +1,6 @@
 jQuery(document).ready(function() {
         jQuery("li.comment p").each(function( index ) {
           jQuery(this).html(jQuery(this).html().replace(":)", '<img src="https://emojicodes.com/wp-content/uploads/2014/02/smile.png" />'));
+          jQuery(this).html(jQuery(this).html().replace(":(", '<img src="http://emojicodes.com/wp-content/uploads/2014/02/frown.png" />'));
         });
       });
